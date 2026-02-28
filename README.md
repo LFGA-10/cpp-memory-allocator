62d95e6b99f320fad9ef23005f465a8c46215a41
LFGA-10 <braiselala@gmail.com>
2026-02-28 22:06:46 +0200

    Initialize cpp-memory-allocator

---
+++ README.md
@@ -0,0 +1,3 @@
+# cpp-memory-allocator
+
+A custom fast memory allocator for C++ applications.
---
+++ main.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+int main() { std::cout << "Starting cpp-memory-allocator"; return 0; }
